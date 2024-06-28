d9fe37582a982755765fc3a25d65f6561495ba11
Tóth Balázs <grandmastar27@gmail.com>
2024-06-28 10:41:55 +0200

    Added gradient to "Dataset" node

--- app/flow editor/node-edges.js
+++ app/flow editor/node-edges.js
@@ -1,15 +1,18 @@
 import { MarkerType } from "reactflow";
+import "./node-edges.style.css";
+
 const position = { x: 0, y: 0 };
 export const initialNodes = [
   {
     id: "Dataset",
     data: { label: "Dataset", sourceCount: 0, targetCount: 1, isInitial: true },
     position,
-    style: {
-      borderRadius: "16px",
-      background: "linear-gradient(135deg, #A2E6AE, #37AC5C)",
-      color: "#fff",
-    },
+    // style: {
+    //   borderRadius: "16px",
+    //   background: "linear-gradient(135deg, #0B4ADB, #09C1FD)",
+    //   color: "#fff",
+    // },
+    className: "animated-gradient",
     type: "selectorNode",
   },
 ];
